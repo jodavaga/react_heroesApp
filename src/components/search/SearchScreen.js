6cdad0d3de958a5e25afd1034c13455ab76c4c94
Jose D. Vasquez <jodavaga@Joses-iMac.local>
2020-08-03 19:18:32 -0500

    feat: create selector by name. improve search hero

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import queryString from 'query-string';
-import { heroes } from '../../data/heroes';
 import { HeroCard } from '../heroes/HeroCard';
 import { useForm } from '../../hooks/useForm';
 import { useLocation } from 'react-router-dom';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
 export const SearchScreen = ({ history }) => {
 
+    // parse location to extract searched text
     const location = useLocation();
     const { q = '' } = queryString.parse(location.search);
     
     
-    const [ { searchText }, handleInputChange, reset ] = useForm({
+    const [ { searchText }, handleInputChange ] = useForm({
         searchText: q
     });
+
     
-    const heroesFiltered = heroes.filter( hero => hero.superhero.toLowerCase().includes( q.toLowerCase().trim() ) );
+    // filter by search text
+    //const heroesFiltered = getHeroesByName( searchText );
+    const heroesFiltered = useMemo(() => getHeroesByName( q ), [ q ]);
     
     const handleSearch = (e) => {
         e.preventDefault();
         
         // navigate to
         history.push(`?q=${ searchText }`);
-
-        // reset form
-        // reset();
     }
 
     return(
@@ -43,6 +44,7 @@ export const SearchScreen = ({ history }) => {
                             name="searchText"
                             value={ searchText }
                             onChange={ handleInputChange }
+                            onClick={ (e) => e.target.select() }
                         />
 
                         <button
@@ -56,6 +58,15 @@ export const SearchScreen = ({ history }) => {
                 </div>
             
                 <div className="col-md-8">
+
+                    { (!q) && <div className="alert alert-info">
+                        Search heroe...
+                    </div>}
+
+                    { (q && !heroesFiltered.length) && <div className="alert alert-warning">
+                        There is no Hero with: <b>{ q }</b>
+                    </div>}
+
                     {
                         heroesFiltered.map( hero => ( 
                             <HeroCard 
